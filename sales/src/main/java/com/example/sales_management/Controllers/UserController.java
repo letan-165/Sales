b8f69696026b27129310f67faed8dab7ac7fabe0
letan-165 <2251160057@ut.edu.vn>
2024-10-18 03:12:55 +0700

    update

--- sales/src/main/java/com/example/sales_management/Controllers/UserController.java
+++ sales/src/main/java/com/example/sales_management/Controllers/UserController.java
@@ -12,18 +12,18 @@ import com.example.sales_management.Models.User;
 import com.example.sales_management.Services.UserService;
 
 @Controller
-public class HomeController {
+public class UserController {
 
     @Autowired
     private UserService userService;
-
+    //Hiển thị danh sách
     @GetMapping("/")
     public String getAllUsers(Model model) {
         
         model.addAttribute("users", userService.findAll());
         return "list";
     }
-
+    //Thêm người dùng
     @GetMapping("/add")
     public String showAddUserForm(Model model) {
         model.addAttribute("user", new User());
@@ -34,7 +34,7 @@ public class HomeController {
         userService.save(user);
         return "redirect:/";
     }
-
+    //Sửa người dùng 
     @GetMapping("/edit/{id}")
     public String editUser(@PathVariable String id, Model model) {
         User user = userService.findById(id);
@@ -46,7 +46,7 @@ public class HomeController {
         userService.updateUser(user);
         return "redirect:/";
     }
-    
+    //Xóa người dùng
     @PostMapping("/delete/{id}")
     public String deleteUser(@PathVariable String id) {
         userService.deleteById(id);
