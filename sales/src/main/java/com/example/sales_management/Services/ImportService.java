5c12659118611c72de776e760e6368209d9cf1ee
dingdong <134074574+TranPhu24@users.noreply.github.com>
2024-11-14 10:49:24 +0700

    Update ImportService.java

--- sales/src/main/java/com/example/sales_management/Services/ImportService.java
+++ sales/src/main/java/com/example/sales_management/Services/ImportService.java
@@ -1,6 +1,9 @@
 package com.example.sales_management.Services;
 
 import java.util.List;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Service;
 
@@ -49,6 +52,37 @@ public class ImportService {
     }
     public void deleteImportProductsByImportID(Long importID) {
         importProductRepository.deleteByImportID(importID);
+    }
+
+    public List<ImportProduct> findImportProductsByTimeRange(String reportType, String startDate, String endDate) {
+        try {
+            LocalDateTime startDateTime = parseStartDate(startDate);
+            LocalDateTime endDateTime = parseEndDate(endDate);
+            return importProductRepository.findByImportTimeBetween(startDateTime, endDateTime);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Định dạng yyyy-MM.", e);
+        }
+    }
+    
+
+    private LocalDateTime parseStartDate(String startDate) {
+        DateTimeFormatter yearMonthFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
+        try {
+            YearMonth startYearMonth = YearMonth.parse(startDate, yearMonthFormatter);
+            return startYearMonth.atDay(1).atStartOfDay();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Định dạng yyyy-MM.");
+        }
+    }
+
+    private LocalDateTime parseEndDate(String endDate) {
+        DateTimeFormatter yearMonthFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
+        try {
+            YearMonth endYearMonth = YearMonth.parse(endDate, yearMonthFormatter);
+            return endYearMonth.atEndOfMonth().atTime(23, 59, 59);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Định dạng yyyy-MM.");
+        }
     }
 
     @Transactional
