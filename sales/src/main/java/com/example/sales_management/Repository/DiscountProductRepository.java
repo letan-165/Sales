977aa257580af67a3a3d90fc6fb6e70efed44fe7
Yến Trân <tranlamyentran@gmail.com>
2024-11-14 22:29:34 +0700

    Update DiscountProductRepository.java

--- sales/src/main/java/com/example/sales_management/Repository/DiscountProductRepository.java
+++ sales/src/main/java/com/example/sales_management/Repository/DiscountProductRepository.java
@@ -1,11 +1,12 @@
 package com.example.sales_management.Repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 import com.example.sales_management.Models.DiscountProduct;
 import com.example.sales_management.Models.DiscountProductId;
@@ -13,7 +14,7 @@ import com.example.sales_management.Models.DiscountProductId;
 import jakarta.transaction.Transactional;
 
 @Repository
-public interface DiscountProductRepository extends JpaRepository<DiscountProduct, DiscountProductId> {
+public interface DiscountProductRepository extends JpaRepository<DiscountProduct, Long> {
     List<DiscountProduct> findByDiscounts_DiscountID(String discountID);
     @Modifying
     @Transactional
