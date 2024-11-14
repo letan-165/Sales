f08bd1958f75573d2b2262056b343eb0cba49f8e
dingdong <134074574+TranPhu24@users.noreply.github.com>
2024-11-14 10:47:13 +0700

    Update ImportProductRepository.java

--- sales/src/main/java/com/example/sales_management/Repository/ImportProductRepository.java
+++ sales/src/main/java/com/example/sales_management/Repository/ImportProductRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.sales_management.Models.ImportProduct;
@@ -21,4 +22,15 @@ public interface ImportProductRepository extends JpaRepository<ImportProduct, Lo
     @Query("DELETE FROM ImportProduct ip WHERE ip.imports.importID = :importID")
     void deleteByImportID(Long importID);
     void deleteById(ImportProductId id);
+
+        @Query("""
+        SELECT ip 
+        FROM ImportProduct ip
+        JOIN ip.imports i
+        WHERE i.time BETWEEN :startDate AND :endDate
+        """)
+    List<ImportProduct> findByImportTimeBetween(
+            @Param("startDate") LocalDateTime startDate, 
+            @Param("endDate") LocalDateTime endDate
+    );
 }
