e31551af503ed2a63f0ffdf7f966d410a07c4cd2
dingdong <134074574+TranPhu24@users.noreply.github.com>
2024-11-19 21:46:34 +0700

    Update OrderService.java

--- sales/src/main/java/com/example/sales_management/Services/OrderService.java
+++ sales/src/main/java/com/example/sales_management/Services/OrderService.java
@@ -1,5 +1,8 @@
 package com.example.sales_management.Services;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -73,5 +76,35 @@ public class OrderService {
     }
     public List<Discount> findDiscountsByProductID(Long price){
         return orderProductRepository.findDiscountsByProductID(price);
+    }
+
+     public List<OrderProduct> findOrderProductsByTimeRange(String startDate, String endDate) {
+        try {
+            LocalDateTime startDateTime = parseStartDate(startDate);
+            LocalDateTime endDateTime = parseEndDate(endDate);
+            return orderProductRepository.findOrderProductsByTimeRange(startDateTime, endDateTime);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Định dạng ngày không hợp lệ. Định dạng hợp lệ: yyyy-MM-dd.", e);
+        }
+    }
+    
+    private LocalDateTime parseStartDate(String startDate) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate localDate = LocalDate.parse(startDate, dateFormatter);
+            return localDate.atStartOfDay();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Định dạng ngày không hợp lệ. Định dạng hợp lệ: yyyy-MM-dd.");
+        }
+    }
+    
+    private LocalDateTime parseEndDate(String endDate) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate localDate = LocalDate.parse(endDate, dateFormatter);
+            return localDate.atTime(23, 59, 59);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Định dạng ngày không hợp lệ. Định dạng hợp lệ: yyyy-MM-dd.");
+        }
     }
 }
